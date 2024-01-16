869e067eafaaf8762bed3d6fd2fdcb1e799c8e74
cysof <cp4reallife@yahoo.com>
2024-01-16 12:13:06 -0800

    cmd fix

--- console.py
+++ console.py
@@ -1,14 +1,25 @@
 #!/usr/bin/python3
 
 import cmd
+import models
 from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.amenity import Amenity
+from models.review import Review
 from models import storage
 
 class HBNBCommand(cmd.Cmd):
 
     prompt = "(hbnb)"
 
-    CLASS_MAP = {"BaseModel":BaseModel}
+    #list of all class
+
+    CLASS_MAP = {"BaseModel": BaseModel, "User": User,
+            "State": State, "City": City, "Place": Place,
+            "Amenity": Amenity, "Review": Review}
 
     def do_quit(self, arg):
         """
